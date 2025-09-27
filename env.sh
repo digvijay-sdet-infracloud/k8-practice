61d563f595722e9ef2a43cbda04df62a88708e1d
digvijay-sdet-infracloud <v-digvijay@exostellar.ai>
2025-09-27 11:39:44 +0530

    added taint & toleration

---
+++ env.sh
@@ -0,0 +1,3 @@
+echo "I am test1"
+echo "I am test2"
+echo "I am test3"
---
+++ secret-file.sh
@@ -0,0 +1,7 @@
+echo "Store All the Info"
+
+DB_USERNAME=admin
+DB_PASSWORD=password
+DB_DATABASE=customer_db
+DB_PORT=3306
+DB_HOST=localhost
